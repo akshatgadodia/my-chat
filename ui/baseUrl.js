fb32d2363cf9b85189e12c075ea409ae2a354a7f
akshatgadodia <akshatgadodia@gmail.com>
2022-12-23 13:11:10 +0530

    cors added

--- ui/baseUrl.js
+++ ui/baseUrl.js
@@ -1,2 +1,3 @@
-const baseUrl = "http://192.168.29.39:5000";
+// const baseUrl = "http://192.168.29.39:5000";
+const baseUrl = "https://my-chat-ip08jzyad-akshatgadodia-gmailcom.vercel.app";
 export default baseUrl;
